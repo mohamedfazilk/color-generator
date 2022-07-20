b0cb00149301aef07e3e488b107888b6097ccd48
mohamedfazilk <2114921@cognizant.com>
2022-07-20 23:05:18 +0530

    setting bcg colors by separating rgb valuesby join method

--- src/Single.js
+++ src/Single.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb,weight,type}) => {
+  const [alert, setAlert] = useState(false);//for alert when copying values
+  const bcg = rgb.join(',')//se[parating rgb values by join method
+  console.log(bcg)
+  return <article className={`color`}
+  style={{backgroundColor:`rgb(${bcg})`}}>
+
+  </article>
 }
 
 export default SingleColor
