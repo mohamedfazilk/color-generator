13a70ef4c1e7f9d2bc2e91bbd1edd86b7a92e670
mohamedfazilk <2114921@cognizant.com>
2022-07-20 23:08:21 +0530

    Adding weight prop

--- src/Single.js
+++ src/Single.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = ({rgb,weight,type}) => {
+const SingleColor = ({rgb,weight, indez, hex}) => {
   const [alert, setAlert] = useState(false);//for alert when copying values
   const bcg = rgb.join(',')//se[parating rgb values by join method
   console.log(bcg)
   return <article className={`color`}
   style={{backgroundColor:`rgb(${bcg})`}}>
+
+    <p>{weight}%  </p>
+    <p>{hex}</p>
 
   </article>
 }
