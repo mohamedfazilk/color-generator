ef9f4f444efee9fc7adfeeca279433bcebc7b855
mohamedfazilk <2114921@cognizant.com>
2022-07-20 23:15:03 +0530

    displaying hexvalues of colors

--- src/Single.js
+++ src/Single.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = ({rgb,weight, indez, hex}) => {
+const SingleColor = ({rgb,weight, index}) => {
   const [alert, setAlert] = useState(false);//for alert when copying values
   const bcg = rgb.join(',')//se[parating rgb values by join method
   console.log(bcg)
+  const hex = rgbToHex(...rgb);
   return <article className={`color`}
   style={{backgroundColor:`rgb(${bcg})`}}>
 
-    <p>{weight}%  </p>
+    <p className='percent-value'>{weight}%  </p>
     <p>{hex}</p>
 
   </article>
